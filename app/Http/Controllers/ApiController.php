8ac6f6d80bd8da2899938e336a917bbc7142e6e5
unknown <nickarsenal007@gmail.com>
2019-11-29 15:33:12 +0545

    Updated api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -48,33 +48,103 @@ class ApiController extends Controller
 		}
 
         $all = DB::table('question_sets')->select('category_id','no_of_question')->where('question_name', $ques->setname)->get();
+
+        $para = DB::table('question_sets')->select('no_of_question')->where('question_name', $ques->setname)->where('category_id', 1000)->first();
         
+        $filter2 = [];
+        if ( $para ) {
+        	
+        	$comprehensive = Paragraph::where('level', '<=', $chklevel)->inRandomOrder()->get()->take( $para->no_of_question );
+
+
+        	if ( count( $comprehensive ) > 0 ) {
+
+	        	foreach ($comprehensive as $key => $value) {
+	        		
+	        		$filter2[$key] = $value;
+	        		
+
+
+	        		$select = [
+							'posts.id', 'post_name', 'category_name', 'option_a', 'option_b',
+							'option_c', 'option_d', 'correct_option', 'explanation', 'level',
+							'posts.created_at', 'posts.updated_at'
+						];
+
+					$allposts = $value->posts()->select( $select )
+										->where('level', '<=', $chklevel)
+										->get();
+
+	        		$filter2[$key]['questions'] = $allposts;
+	        		$filter2[$key]['category_id'] = 1000;
+	        		$filter2[$key]['post_name'] = '';
+	        		$filter2[$key]['featured'] = NULL;
+	        		$filter2[$key]['category_name'] = '';
+	        		$filter2[$key]['option_a'] = '';
+	        		$filter2[$key]['option_b'] = '';
+	        		$filter2[$key]['option_c'] = '';
+	        		$filter2[$key]['option_d'] = '';
+	        		$filter2[$key]['correct_option'] = '';
+	        		$filter2[$key]['explanation'] = '';
+	        		$filter2[$key]['option_d'] = '';
+	        		$filter2[$key]['option_d'] = '';
+
+	        		$filter2[$key]->type = "1"; 
+
+
+	        	}
+        	}
+
+        }
+
         $data = [];
         foreach ( $all as $val ) {
 
         	$posts = DB::table('posts')->where('category_id', $val->category_id);
 
         	if ( $posts->count() > 0 ) {
-        		if ( $val->no_of_question ) {
+        		if ( $val->no_of_question && $val->category_id !== 1000 ) {
 		            $data[] = DB::table('posts')->where('category_id', $val->category_id)
-		            	->where('level', '<=', $chklevel)->inRandomOrder()->get()->take( $val->no_of_question )->toArray();
+		            	->where('level', '<=', $chklevel)->inRandomOrder()->get()->take( $val->no_of_question );
 		          }
         	}
 
         }
 
-        $filter = call_user_func_array('array_merge', $data);
+		$filter1 = collect( $data )->flatten();
 
-		return response()->json( ['data' => $filter, 'questioncount' => count($filter) ]);
+        foreach ($filter1 as $key => $value) {
+        	$filter[$key] = $value;
+        	$filter[$key]->type = 0;
+        	$filter[$key]->comprehensive_categories_id = '';
+        	$filter[$key]->title = '';
+        	$filter[$key]->paragraph = '';
+        	$filter[$key]->questions = [];
+        }
+
+        if ( $filter2 ) {
+        	return response()->json( ['data' => array_merge( $filter2 , $filter), 'questioncount' => count(array_merge( $filter2 , $filter)) ]);
+        }else{
+        	return response()->json( ['data' => $filter, 'questioncount' => count($filter) ]);
+        }
 
 	}
 
 	public function getCategories(){
 
+
+
 		$cats = MainCategory::all();
 
 		$data = [];
-		$i = 0;
+
+		$data[0]['id'] = 1000;
+		$data[0]['main_category_name'] = 'Comprehensive;';
+		$data[0]['slug'] = 'comprehensive';
+		$data[0]['featured'] = 1;
+		$data[0]['subcategories'] = url('/api/getComprehensiveCategories');
+
+		$i = 1;
 		foreach ($cats as $key) {
 			
 			$data[$i]['id'] = $key->id;
@@ -93,6 +163,10 @@ class ApiController extends Controller
 	public function getComprehensiveCategories(){
 
 		$cats = ComprehensiveCategories::orderBy('id', 'DESC')->get();
+
+		if ( count($cats) == 0 ) {
+			return response()->json(['message' => 'No categories at the moment.']);
+		}
 
 		$data = [];
 		$i = 0;
